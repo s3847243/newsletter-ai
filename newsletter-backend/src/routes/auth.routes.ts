3888a72c88dfb2bf7da5a068a2fa796a20d389bf
Hibbaan <s3847243@student.rmit.edu.au>
2025-12-28 22:05:37 +1100

    routes added for logout and me

--- newsletter-backend/src/routes/auth.routes.ts
+++ newsletter-backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, register,refreshTokenHandler,verifyEmail,resendVerifyEmail   } from "../controllers/auth.controller";
+import { login, register,refreshTokenHandler,verifyEmail,resendVerifyEmail ,logout,me  } from "../controllers/auth.controller";
 import { forgotPassword } from "../controllers/forgot-password.controller";
 import { resetPassword } from "../controllers/reset-password.controller";
 export const authRouter = Router();
@@ -9,6 +9,8 @@ authRouter.post("/login", login);
 authRouter.post("/refresh", refreshTokenHandler);
 authRouter.post("/forgot-password", forgotPassword);
 authRouter.post("/reset-password", resetPassword);
+authRouter.post("/logout", logout);       
+authRouter.get("/me", me);                
 authRouter.post("/verify-email", verifyEmail);
 authRouter.post("/resend-verify-email", resendVerifyEmail);
 
